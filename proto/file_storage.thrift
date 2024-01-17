442b17dd4e84bb08bab8842e002d1a54dbe88aef
Gregory <32060161+ggmaleva@users.noreply.github.com>
2024-01-17 12:36:01 +0300

    add multipart upload (#35)
    * add multipart upload
    
    * rename
    
    * typo
    
    * fix review
    
    * fix review
    
    ---------
    
    Co-authored-by: ggmaleva <ggmaleva@yandex.ru>

--- proto/file_storage.thrift
+++ proto/file_storage.thrift
@@ -8,10 +8,14 @@ namespace erlang filestore.storage
 typedef base.Timestamp Timestamp
 // id файла
 typedef base.ID FileDataID
+// id передачи файла по частям
+typedef base.ID MultipartUploadID
 // имя файла
 typedef string FileName
 // ссылка на файл
 typedef string URL
+// id переданной части файла
+typedef base.ID PartID
 // дополнительная информация о файле
 typedef map<string, msgpack.Value> Metadata
 
@@ -33,6 +37,54 @@ struct NewFileResult {
     1: required FileDataID file_data_id
     // ссылка на файл для дальнейшей выгрузки на сервер
     2: required URL upload_url
+}
+
+struct CreateMultipartUploadResult {
+    // id файла
+    1: required FileDataID file_data_id
+    // id передачи файла по частям
+    2: required MultipartUploadID multipart_upload_id
+}
+
+struct UploadMultipartRequestData {
+    // id файла
+    1: required FileDataID file_data_id
+    // id передачи файла по частям
+    2: required MultipartUploadID multipart_upload_id
+    // номер передаваемой части файла
+    3: required i32 sequence_part
+    // содержимое части файла
+    4: required binary content
+    // размер части файла в байтах
+    5: required i32 content_length
+}
+
+struct UploadMultipartResult {
+    // id части файла
+    1: required PartID part_id
+    // номер переданной части файла
+    2: required i32 sequence_part
+}
+
+struct CompletedMultipart {
+    // id части файла
+    1: required PartID part_id
+    // номер переданной части файла
+    2: required i32 sequence_part
+}
+
+struct CompleteMultipartUploadRequest {
+    // id файла
+    1: required FileDataID file_data_id
+    // id передачи файла по частям
+    2: required MultipartUploadID multipart_upload_id
+    // список id переданных частей файла
+    3: required list<CompletedMultipart> completed_parts
+}
+
+struct CompleteMultipartUploadResult {
+    // ссылка на файл
+    1: required URL upload_url
 }
 
 /*
@@ -71,5 +123,31 @@ service FileStorage {
     * */
     FileData GetFileData (1: FileDataID file_data_id)
         throws (1: FileNotFound ex1)
+
+    /*
+    * Создать новую загрузку файла по частям
+    * metadata - данные о файле, которые сохраняются как метаданные при создании нового файла
+    *
+    * Возвращает идентификатор частичной загрузки файла
+    * */
+    CreateMultipartUploadResult CreateMultipartUpload (1: Metadata metadata)
+
+    /*
+    * Загрузка части файла на сервер
+    * upload_multipart_request_data - данные части файла: идентификатор файла, идентификатор частичной загрузки, ее соджержимое,
+    * размер содержимого.
+    *
+    * Возвращает данные для идентификации части в загружаемом файле
+    * */
+    UploadMultipartResult UploadMultipart (1: UploadMultipartRequestData upload_multipart_request_data)
+
+    /*
+    * Завершение загрузки файла по частям и генерация ссылки на файл
+    * complete_multipart_upload_request - данные о загруженных частях файла: идентификатор файла, идентификатор частичной загрузки
+    * и список метаинформации о переданных частях файла
+    *
+    * Возвращает ссылку на файл
+    * */
+    CompleteMultipartUploadResult CompleteMultipartUpload (1: CompleteMultipartUploadRequest complete_multipart_upload_request)
 
 }
